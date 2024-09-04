d5e509a8aa4e95f3d65cfc3fa57d3a67e8fc8449
MubasharHussain05 <148677944+MubasharHussain05@users.noreply.github.com>
2024-09-04 14:48:13 +0500

    deploy react to github

--- src/API/GetApiData.jsx
+++ src/API/GetApiData.jsx
@@ -1,7 +1,7 @@
 export const ApiData=async()=>{
     try{
         // const response=await fetch  ('http://www.omdbapi.com/?i=tt3896198&apikey=82972e02')
-        const response=await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=82972e02&s=titanic&page=1`)
+        const response=await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=82972e02&s=titanic&page=1`)
         const Data=response.json()
         console.log(Data)
         return Data
--- src/API/GetMovieDetails.jsx
+++ src/API/GetMovieDetails.jsx
@@ -3,7 +3,7 @@ export const GetMovieDetail=async({params})=>{
     const id=params.movieID
     try{
         // const response=await fetch  ('http://www.omdbapi.com/?i=tt3896198&apikey=82972e02')
-        const response=await fetch(`http://www.omdbapi.com/?i=${id}&apikey=82972e02`)
+        const response=await fetch(`https://www.omdbapi.com/?i=${id}&apikey=82972e02`)
         const Data=response.json()
         console.log(Data)
         return Data
